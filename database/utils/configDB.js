9350eb86045aa1fe32b987baaa053c3c78abbe12
Ivan Morales <IvanJohnMorales@Gmail.com>
2022-05-10 12:13:10 -0400

    Initial commit

---
+++ database/utils/configDB.js
@@ -0,0 +1,15 @@
+/*==================================================
+/database/utils/configDB.js
+It declares and exports the variables for database name, username, and password.
+==================================================*/
+// Declare the variables for database name, username, and password.
+const dbName = 'WebDev CRUD App';
+const dbUser = 'postgres';
+const dbPwd = 'Morales13';
+
+// Export the variables 
+module.exports = {
+  dbName,
+  dbUser,
+  dbPwd
+};
